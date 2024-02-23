237c799d22b69575b9fcdde79a58fe15fa766d5e
Yoni Baciu <yoni.baciu@persado.com>
2024-02-22 22:13:20 -0500

    initial chat server and client

---
+++ chat-server/src/messages/messages.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@nestjs/common';
+import { CreateMessageDto } from './dto/create-message.dto';
+import { UpdateMessageDto } from './dto/update-message.dto';
+import { Message } from './entities/message.entity';
+
+@Injectable()
+export class MessagesService {
+  messages: Message[] = [{ name: 'Yoyo', text: 'bruh...'}];
+  clientToUser = {};
+
+  identify(name: string, clientId: string) {
+    this.clientToUser[clientId] = name;
+
+    return Object.values(this.clientToUser);
+  }
+
+  getClientName(clientId: string) {
+    return this.clientToUser[clientId];
+  }
+
+  create(createMessageDto: CreateMessageDto, clientId: string) {
+    const message = {
+      name: this.clientToUser[clientId],
+      text: createMessageDto.text,
+    };
+    this.messages.push(message);
+
+    return message;
+  }
+
+  findAll() {
+    return this.messages;
+  }
+}
